e5ae9d7edfe489660e4a13b9c9f73b9dd7f6a32d
Divya Garg <dgag0811@gmail.com>
2025-04-13 00:13:36 -0400

    Create korean_hindi.py

---
+++ korean_hindi.py
@@ -0,0 +1,19 @@
+# Korean Vowels and Consonants
+# TODO: Add English pronunciation as well
+
+korean_to_hindi = {
+    
+    'ㅗ': 'ओ',
+    'ㅜ': 'ऊ',
+    'ㅡ': 'उ (flat)',
+    'ㅣ': 'इ',
+    'ㅐ': 'ऐ', 
+    'ㅔ': 'ए'
+}
+
+# Korean words
+korean_vocab = {
+}
+
+# --- Functions to look up your learnings ---
+
